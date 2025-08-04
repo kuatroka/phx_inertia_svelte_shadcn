31f76ecf6ab4e74d0ab068ca636fac022ea579a3
kuatroka <aaarmani_2000@yahoo.com>
2025-08-04 16:28:17 +0200

    fix: remove deprecated phoenix compiler and add Ecto.Query require

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule DemoPhoenixInertiaSvelte.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :phoenix_live_view | Mix.compilers()],
+      compilers: [:phoenix_live_view | Mix.compilers()],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
